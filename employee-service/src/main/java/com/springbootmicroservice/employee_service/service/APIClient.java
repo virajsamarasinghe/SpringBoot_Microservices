0c41aa9b06812d872e788f78c481f2ca762cad18
Viraj Samarasinghe <123957527+virajsamarasinghe@users.noreply.github.com>
2025-02-04 18:59:56 +0530

    api-gateway final commite

--- employee-service/src/main/java/com/springbootmicroservice/employee_service/service/APIClient.java
+++ employee-service/src/main/java/com/springbootmicroservice/employee_service/service/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(url = "http://localhost:9090", name = "DEPARTMENT-SERVICE")
-@FeignClient(name = "DPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("api/v1/departments/{code}")
